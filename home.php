5ec322252e6f9ba3d3a102a32a22ab32556661ee
Shayan Ahmad <shayanahmad235@gmail.com>
2024-08-15 20:27:58 +0500

    after login home page are created

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+require './config/connection.php';
+require_once('./config/security.php');
+
+require './include/head.php';
+
+require './include/header.php';
+?>
+
+<main>
+    <section>
+        <h2>Welcome to My Website</h2>
+        <p>This is a simple responsive website layout example. Resize the browser window to see how the layout changes.</p>
+    </section>
+</main>
+
+<?php require './include/footer.php'; ?>
